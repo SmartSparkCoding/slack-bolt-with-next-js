bfeade79be34d38017a595751483cf8055de85c4
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-03-27 20:28:14 +0000

    Register mini-jacob-test command in index.ts

--- src/lib/bolt/listeners/commands/index.ts
+++ src/lib/bolt/listeners/commands/index.ts
@@ -1,8 +1,10 @@
 import type { App } from "@slack/bolt";
 import sampleCommandCallback from "./sample-command";
+import miniJacobTestCommand from "./mini-jacob-test";
 
 const register = (app: App) => {
   app.command("/sample-command", sampleCommandCallback);
+  app.command("/mini-jacob-test", miniJacobTestCommand);
 };
 
 export default { register };
