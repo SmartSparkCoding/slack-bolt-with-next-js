06c4c02d5a147b840c7ed34c30b7d90d8bda4e2c
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-03-27 21:10:44 +0000

    Add jacobMentionListener to message listeners

--- src/lib/bolt/listeners/messages/index.ts
+++ src/lib/bolt/listeners/messages/index.ts
@@ -1,10 +1,12 @@
 import type { App } from "@slack/bolt";
 import { sampleMessageCallback } from "./sample-message";
 import botMentionListener from "./bot-mention";
+import jacobMentionListener from "./jacob-mention";
 
 const register = (app: App) => {
   app.message(/^hello.*/, sampleMessageCallback);
   app.event("app_mention", botMentionListener);
+  app.message(jacobMentionListener);
 };
 
 export default { register };
