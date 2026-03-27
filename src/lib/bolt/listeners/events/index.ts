f2f8cd6b53a0e5b832c932a4fd94048a9a311454
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-03-27 20:53:45 +0000

    Register userJoinedChannel event handler

--- src/lib/bolt/listeners/events/index.ts
+++ src/lib/bolt/listeners/events/index.ts
@@ -1,8 +1,10 @@
 import type { App } from "@slack/bolt";
 import { appHomeOpenedCallback } from "./app-home-opened";
+import userJoinedChannel from "./user-joined-channel";
 
 const register = (app: App) => {
   app.event("app_home_opened", appHomeOpenedCallback);
+  app.event("member_joined_channel", userJoinedChannel);
 };
 
 export default { register };
