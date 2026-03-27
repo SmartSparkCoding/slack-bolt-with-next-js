39d134249363307255ac397ce4662b567cba9935
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-03-27 20:45:18 +0000

    Add bot mention listener to message registration

--- src/lib/bolt/listeners/messages/index.ts
+++ src/lib/bolt/listeners/messages/index.ts
@@ -1,8 +1,10 @@
 import type { App } from "@slack/bolt";
 import { sampleMessageCallback } from "./sample-message";
+import botMentionListener from "./bot-mention";
 
 const register = (app: App) => {
   app.message(/^hello.*/, sampleMessageCallback);
+  app.event("app_mention", botMentionListener);
 };
 
 export default { register };
