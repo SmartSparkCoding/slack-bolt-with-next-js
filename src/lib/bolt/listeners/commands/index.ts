46939fb5f6ce531f9c2d6d5034ce154b5dc84996
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-03-27 20:34:59 +0000

    Add miniJacobSocials command registration

--- src/lib/bolt/listeners/commands/index.ts
+++ src/lib/bolt/listeners/commands/index.ts
@@ -1,10 +1,12 @@
 import type { App } from "@slack/bolt";
 import sampleCommandCallback from "./sample-command";
 import miniJacobTestCommand from "./mini-jacob-test";
+import miniJacobSocialsCommand from "./mini-jacob-socials";
 
 const register = (app: App) => {
   app.command("/sample-command", sampleCommandCallback);
   app.command("/mini-jacob-test", miniJacobTestCommand);
+  app.command("/mini-jacob-socials", miniJacobSocialsCommand);
 };
 
 export default { register };
