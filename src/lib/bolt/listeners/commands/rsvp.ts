cd95d837f68a317c0ed37de0d300a71235539a27
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-04-02 15:16:32 +0100

    Refactor RSVP command registration into a function

--- src/lib/bolt/listeners/commands/rsvp.ts
+++ src/lib/bolt/listeners/commands/rsvp.ts
@@ -1,18 +1,18 @@
-await app.command("/mini-jacob-rsvp", async ({ ack, client }) => {
-  await ack();
+export default function registerRSVPCommand(app: any) {
+  app.command("/mini-jacob-rsvp", async ({ ack, client }: any) => {
+    await ack();
 
-  const message = await getRSVPMessage();
+    const message = await getRSVPMessage();
 
-  const userId = "U0AEYDUCLKF"; 
-  
-  // Open DM
-  const dm = await client.conversations.open({
-    users: userId
+    const userId = "U0AEYDUCLKF";
+
+    const dm = await client.conversations.open({
+      users: userId,
+    });
+
+    await client.chat.postMessage({
+      channel: dm.channel.id,
+      text: message,
+    });
   });
-
-  // Send message
-  await client.chat.postMessage({
-    channel: dm.channel.id,
-    text: message
-  });
-});
+}
