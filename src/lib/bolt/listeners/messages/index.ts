3b9fe5263fdedadde6e4a479c86936b9a00b8459
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-04-11 22:47:36 +0100

    Add triggerHackanomous listener to message events

--- src/lib/bolt/listeners/messages/index.ts
+++ src/lib/bolt/listeners/messages/index.ts
@@ -1,10 +1,12 @@
 import type { App } from "@slack/bolt";
 import botMentionListener from "./bot-mention";
 import jacobMentionListener from "./jacob-mention";
+import triggerHackanomous from "./hackanomous";
 
 const register = (app: App) => {
   app.event("app_mention", botMentionListener);
   app.message(jacobMentionListener);
+  app.message(triggerHackanomous);
 };
 
 export default { register };
